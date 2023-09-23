12303bb5a6a9149c8ebf916178790fc8a8be26cc
bavimane <bavimaneharishgowdru@gmail.com>
2023-09-23 10:22:37 +0530

    segregated data

--- src/Components/DesignList.js
+++ src/Components/DesignList.js
@@ -1,91 +1,29 @@
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 import InfoIcon from "@mui/icons-material/Info";
-import IconButton from "@mui/material/IconButton";
-import { ImageListItemBar } from "@mui/material";
-import Badge from "@mui/material/Badge";
+import {
+  ImageListItemBar,
+  Badge,
+  IconButton,
+  ImageListItem,
+  ImageList,
+} from "@mui/material";
+
+import { DATA } from "./constants";
 
 const DesignList = () => {
-  const data = [
-    {
-      id: 1,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/otlhHiPryHzjBhDcX7mJ2Q/medium_24F__1_.jpg",
-      likes: 2,
-      comments: [],
-    },
-    {
-      id: 2,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/ppVgaTAdU-l_pbfZXHqw7w/medium_23F__1_.jpg",
-      likes: 0,
-      comments: [],
-    },
-    {
-      id: 3,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/tohQXzghpooKZVMmmH04hA/medium_22F__1_.jpg",
-      comments: [],
-    },
-    {
-      id: 4,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/8Yopql2EZmSztJBASV7WzA/medium_16F__1_.jpg",
-      comments: [],
-    },
-    {
-      id: 5,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/LRn9ZWt-G-N_UZjleYxNBQ/medium_15F1.jpg",
-      comments: [],
-    },
-    {
-      id: 6,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/knFzDSOK077cepRrTwdzmA/medium_14F1.jpg",
-      comments: [],
-    },
-    {
-      id: 7,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/hMAvyhIgshzPhtG0m2uwFQ/medium_13F1.jpg",
-      comments: [],
-    },
-    {
-      id: 8,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/Zc92-6ehMtPUN9eAezGcvg/medium_2F1.jpg",
-      comments: [],
-    },
-    {
-      id: 9,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/tohQXzghpooKZVMmmH04hA/medium_22F__1_.jpg",
-      comments: [],
-    },
-    {
-      id: 10,
-      image:
-        "https://sbcdn1.superbolter.com/atom/themes/tohQXzghpooKZVMmmH04hA/medium_22F__1_.jpg",
-      comments: [],
-    },
-  ];
   return (
-    <div>
+    <>
       <ImageList cols={3} gap={8}>
-        {data.map((item) => (
-          <ImageListItem key={item.image}>
+        {DATA.map((item) => (
+          <ImageListItem key={item.id}>
             <img src={item.image} alt="" />
 
             <ImageListItemBar
-              title={item.title}
-              subtitle={item.author}
               actionIcon={
                 <>
                   <IconButton
                     sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                    aria-label={`info about ${item.title}`}
+                    aria-label={`info about ${item.id}`}
                   >
                     <Badge badgeContent={item.likes} color="primary">
                       <ThumbUpAltIcon
@@ -95,7 +33,7 @@ const DesignList = () => {
                   </IconButton>
                   <IconButton
                     sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                    aria-label={`info about ${item.title}`}
+                    aria-label={`info about ${item.id}`}
                   >
                     <InfoIcon style={{ color: "grey", display: "flex" }} />
                   </IconButton>
@@ -105,7 +43,7 @@ const DesignList = () => {
           </ImageListItem>
         ))}
       </ImageList>
-    </div>
+    </>
   );
 };
 
---
+++ src/Components/constants.js
@@ -0,0 +1,66 @@
+const DATA = [
+  {
+    id: 1,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/otlhHiPryHzjBhDcX7mJ2Q/medium_24F__1_.jpg",
+    likes: 2,
+    comments: [],
+  },
+  {
+    id: 2,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/ppVgaTAdU-l_pbfZXHqw7w/medium_23F__1_.jpg",
+    likes: 0,
+    comments: [],
+  },
+  {
+    id: 3,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/tohQXzghpooKZVMmmH04hA/medium_22F__1_.jpg",
+    comments: [],
+  },
+  {
+    id: 4,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/8Yopql2EZmSztJBASV7WzA/medium_16F__1_.jpg",
+    comments: [],
+  },
+  {
+    id: 5,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/LRn9ZWt-G-N_UZjleYxNBQ/medium_15F1.jpg",
+    comments: [],
+  },
+  {
+    id: 6,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/knFzDSOK077cepRrTwdzmA/medium_14F1.jpg",
+    comments: [],
+  },
+  {
+    id: 7,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/hMAvyhIgshzPhtG0m2uwFQ/medium_13F1.jpg",
+    comments: [],
+  },
+  {
+    id: 8,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/Zc92-6ehMtPUN9eAezGcvg/medium_2F1.jpg",
+    comments: [],
+  },
+  {
+    id: 9,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/tohQXzghpooKZVMmmH04hA/medium_22F__1_.jpg",
+    comments: [],
+  },
+  {
+    id: 10,
+    image:
+      "https://sbcdn1.superbolter.com/atom/themes/tohQXzghpooKZVMmmH04hA/medium_22F__1_.jpg",
+    comments: [],
+  },
+];
+
+export { DATA };
